8616997be9e48ed3a4e58a88722bf99d2f67c868
17011118 <17011118@e107m01.istic.univ-rennes1.fr>
2019-02-11 17:31:38 +0100

    ddf

--- project_maven/src/main/java/project_maven/Calculatrice.java
+++ project_maven/src/main/java/project_maven/Calculatrice.java
@@ -1,5 +1,9 @@
 package project_maven;
 
+import javax.swing.*;
+
 public class Calculatrice {
+	String[] element = {"0","1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "=", "C", "+", "-", "*", "/"};
+	JButton[] bouton = new JButton[element.length]; 
 
 }
