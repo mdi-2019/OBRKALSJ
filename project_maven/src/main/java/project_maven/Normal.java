4b835d0ab5c6c8d8b62528e9b76b47ba9c195200
unknown <salomejarnouen@hotmail.fr>
2019-02-11 17:48:36 +0100

    ajout fonctionnalités classe normal

--- project_maven/src/main/java/project_maven/Normal.java
+++ project_maven/src/main/java/project_maven/Normal.java
@@ -1,5 +1,21 @@
 package project_maven;
 
-public class Normal extends Calculatrice{
+public class Normal{
 
+	public static int add(int a, int b) {
+		return (a+b);
+	}
+	
+	public static int minus(int a, int b) {
+		return (a-b);
+	}
+	
+	public static int mult(int a, int b) {
+		return (a*b);
+	}
+	
+	public static int div(int a, int b) {
+		return (a/b);
+	}
+	
 }
